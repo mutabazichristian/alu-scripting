d3b0b0d7ed242a0d743a207088f7d7d99cf4d9de
Mutabazi <113415988+mutabazichristian@users.noreply.github.com>
2023-01-11 11:48:53 +0000

    file 8

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 x = ARGV[0]
 def function(x)
-    puts x.scan(/\[from:.+\]\s\[to:.+\]\s\[flags:(.............)/).join
+    puts x.scan(/\[from:.+\]\s\[to:.+\]\s\[flags:.+-1\]/).join
 end
 
  function(x)
