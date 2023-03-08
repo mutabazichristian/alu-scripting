34a4c32467910761e62fabef2cb19fd656e8a55f
mutabazichristian <c.mutabazi@alustudent.com>
2023-03-08 17:14:22 +0200

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,11 +4,11 @@ I have no idea what they mean by documenting (didn't quiet get it last time but,
 """
 import requests
 
-"""
-Write a function that queries the Reddit API and returns the number of 
-subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
-"""
 def number_of_subscribers(subreddit):
+    """
+    A function that queries the Reddit API and returns the number of 
+    subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
