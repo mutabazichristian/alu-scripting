8cc447555faf8740f3a0e5df1767f6fce7f04a14
mutabazichristian <c.mutabazi@alustudent.com>
2023-03-08 18:14:51 +0200

    task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,15 +5,17 @@ function that returns the titles of the first 10 hot posts listed for a given su
 
 
 import requests
+subreddit = 'boy'
 
 def top_ten(subreddit):
-    url = f'http://reddit.com/r/{subreddit}/about.json'
+    url = f'http://reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent':'Mozila/5.0'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        for [data][children][:10]:
+            return ([post][title])
 
     else:
-        print('damn')
+        return 0
