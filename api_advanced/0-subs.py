8d11f1cac4e8c2b0cce99df541d760a9be833a7b
mutabazichristian <c.mutabazi@alustudent.com>
2023-03-08 17:00:59 +0200

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,5 +2,16 @@
 """
 I have no idea what they mean by documenting (didn't quiet get it last time but, we move!)
 """
+import requests
+
 def number_of_subscribers(subreddit):
-    return 0    
+    url = 'https://www.reddit.com/dev/api/{subreddit}'
+    headers = {'user-Agent': 'Mozilla/5.0'}
+    response = requests.get(url,headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data[data][subscribers]
+
+    else:
+        return 0 
