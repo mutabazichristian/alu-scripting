7139427818d416056af15bc19bf7705e69280228
mutabazichristian <c.mutabazi@alustudent.com>
2023-03-08 17:22:41 +0200

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
-This module provides a function for querying the number of subscribers for a given subreddit on Reddit.
+A script that use reddit api to outputs the
+number of subscribers of a certain subreddit.
 """
 
+
+import json
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    A function that queries the Reddit API and returns the number of 
-    subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
+        data = json.loads(response.text)
         return data['data']['subscribers']
+
     else:
         return 0
-
