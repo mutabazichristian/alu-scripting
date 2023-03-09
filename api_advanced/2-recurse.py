29efeb08465a3374fdbaec8211d1efc6c3ce3a43
mutabazichristian <c.mutabazi@alustudent.com>
2023-03-09 12:14:03 +0200

    task 2

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-Recursive function that queries Reddit API,
-and returns a list containing the titles of all hot
-articles for a given subreddit.
-
-No result returns "None"
+Append all the hot post's titles from a particular
+subreddit on a list calleg hot_list.
 """
 
+import json
 import requests
 
-def recurse(subreddit, hot_list=[],after='None'):
-    url = 'https://reddit.com/r/hot.json'.format('subreddit')
-    headers = {'User-Agent' : 'Mozilla/5.o'}
-    params = {'limit' : 100}
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    # After is set to none at first.
+    params = {'after': after} if after else {}
+
     response = requests.get(url, headers=headers, params=params)
+
     if response.status_code == 200:
         data = response.json()
 
@@ -21,9 +24,15 @@ def recurse(subreddit, hot_list=[],after='None'):
             title = post['data']['title']
             hot_list.append(title)
 
+        # set after to the last post of the previous result.
         after = data['data']['after']
-    if data['after']:
-        return recurse(subreddit, hot_list,data['after'])
+
+        # call the function with the updated after i.e recursion
+        # to retrieve addition pages.
+        if after:
+            recurse(subreddit, hot_list, after)
+
     else:
-        return 'None'
-    
+        return None
+
+    return hot_list
