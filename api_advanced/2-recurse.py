f05cf3a23e8cc4fa4c395ba7d4eaa06abb9bd774
mutabazichristian <c.mutabazi@alustudent.com>
2023-03-09 11:04:23 +0200

    task 2

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries Reddit API,
+and returns a list containing the titles of all hot
+articles for a given subreddit.
+
+No result returns "None"
+"""
+
+import requests
+
+def recurse(subreddit, hot_list=[]):
+    url = 'https://reddit.com/r/hot.json'.format('subreddit')
+    headers = {'User-Agent' : 'Mozilla/5.o'}
+    params = {'limit' : 100}
+
+    if after:
+        params['after'] = params
+        response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200 :
+        return None
+    data = response.json()
+    child = data['children']
+    for child in children:
+        title = child['data']['title']
+        hot_list.append(title)
+    if not data['after']:
+        return recurse(subreddit,hot_list, data['after'])
